995a57a8a0483083b24b2ea75e474fbe9ecab735
Erkebulan <mk.erkebulan@gmail.com>
2021-10-23 23:38:10 +0600

    myApp v1.0.0

---
+++ hw2/myApp/go.mod
@@ -0,0 +1,5 @@
+module github.com/erkkke/one-tech-go/hw2/myApp
+
+go 1.17
+
+require github.com/erkkke/one-tech-go/hw2/myLibrary v0.0.0-20211023173205-1e1343744012
--- hw2/myApp/main.go
+++ hw2/myApp/main.go
@@ -1,7 +1,11 @@
-package myApp
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/erkkke/one-tech-go/hw2/myLibrary"
+)
 
 func main() {
-	fmt.Println()
+	fmt.Println(myLibrary.LowerToUpper("Hello!"))
+	fmt.Println(myLibrary.UpperToLower("Hello!"))
 }
